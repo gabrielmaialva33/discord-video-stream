ce59b49e64465821abc0e204c95954b2ef6ccc57
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-05-27 02:06:21 -0300

    chore: bump libs

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,4 @@
 export * from '#src/client/packet/index'
 export * from '#src/client/voice/index'
-export * from '#src/client/processing/index'
-
 export * from '#src/client/gateway_op_codes'
 export * from '#src/client/streamer'
