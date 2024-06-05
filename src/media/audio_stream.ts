e4d668dfba0c7a2663448682b974347e1634e867
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-06-05 20:44:58 -0300

    feat: add play and pause stream

--- src/media/audio_stream.ts
+++ src/media/audio_stream.ts
@@ -7,6 +7,7 @@ class AudioStream extends Writable {
   private readonly sleepTime: number
   private startTime?: number
   private readonly noSleep: boolean
+  paused: boolean = false
 
   constructor(udp: MediaUdp, noSleep = false, options?: WritableOptions) {
     super(options)
@@ -16,19 +17,38 @@ class AudioStream extends Writable {
     this.noSleep = noSleep
   }
 
-  _write(chunk: any, _: BufferEncoding, callback: (error?: Error | null) => void): void {
+  async _write(
+    chunk: any,
+    _: BufferEncoding,
+    callback: (error?: Error | null) => void
+  ): Promise<void> {
     this.count++
     if (!this.startTime) this.startTime = performance.now()
 
     this.udp.sendAudioFrame(chunk)
 
-    const next = (this.count + 1) * this.sleepTime - (performance.now() - this.startTime)
-
-    if (this.noSleep || next <= 0) {
+    if (this.noSleep) {
       callback()
     } else {
-      setTimeout(callback, next)
+      do {
+        this.count++
+        const next = (this.count + 1) * this.sleepTime - (performance.now() - this.startTime)
+        await this.delay(next)
+      } while (this.paused)
+      callback()
     }
+  }
+
+  delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  pause() {
+    this.paused = true
+  }
+
+  resume() {
+    this.paused = false
   }
 }
 
--- src/media/video_stream.ts
+++ src/media/video_stream.ts
@@ -7,6 +7,7 @@ export class VideoStream extends Writable {
   private sleepTime: number
   private startTime?: number
   private readonly noSleep: boolean
+  paused: boolean = false
 
   constructor(udp: MediaUdp, fps: number = 30, noSleep = false, options?: WritableOptions) {
     super(options)
@@ -20,18 +21,37 @@ export class VideoStream extends Writable {
     this.sleepTime = time
   }
 
-  _write(frame: any, _encoding: BufferEncoding, callback: (error?: Error | null) => void): void {
+  async _write(
+    frame: any,
+    _encoding: BufferEncoding,
+    callback: (error?: Error | null) => void
+  ): Promise<void> {
     this.count++
     if (!this.startTime) this.startTime = performance.now()
 
     this.udp.sendVideoFrame(frame)
 
-    const next = (this.count + 1) * this.sleepTime - (performance.now() - this.startTime)
-
-    if (this.noSleep || next <= 0) {
+    if (this.noSleep) {
       callback()
     } else {
-      setTimeout(callback, next)
+      do {
+        this.count++
+        const next = (this.count + 1) * this.sleepTime - (performance.now() - this.startTime)
+        await this.delay(next)
+      } while (this.paused)
+      callback()
     }
+  }
+
+  delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  pause() {
+    this.paused = true
+  }
+
+  resume() {
+    this.paused = false
   }
 }
