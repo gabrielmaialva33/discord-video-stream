e0ab6703175f76a2eecac0402d0a599ee9f5b58b
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-12-05 18:38:44 -0300

    refactor: how "new_api" is imported and exported.
    Switch from exporting "new_api.js" directly to exporting it under the alias "NewApi." This change enhances module structure by maintaining consistent and clear naming conventions.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './client/index.js'
 export * from './media/index.js'
-export * from './media/new_api.js'
 export * as Utils from './utils.js'
+export * as NewApi from './media/new_api.js'
