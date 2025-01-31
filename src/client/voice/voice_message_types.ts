f92f888d6d1d568b74f8f4b2d3eb9250d0d618fa
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-01-30 22:34:01 -0300

    refactor: types to improve readability and consistency
    Removed trailing commas and reorganized type definitions with clearer formatting and alignment. This enhances overall readability and adheres to consistent TypeScript style practices throughout the file.

--- src/client/voice/voice_message_types.ts
+++ src/client/voice/voice_message_types.ts
@@ -1,12 +1,12 @@
-import {SupportedEncryptionModes} from '../../utils.js'
-import {VoiceOpCodes} from "./voice_op_codes.js";
+import { SupportedEncryptionModes } from '../../utils.js'
+import { VoiceOpCodes } from './voice_op_codes.js'
 
 type StreamInfo = {
-  active: boolean,
-  quality: number,
-  rid: string,
-  ssrc: number,
-  rtx_ssrc: number,
+  active: boolean
+  quality: number
+  rid: string
+  ssrc: number
+  rtx_ssrc: number
   /**
    * always "video" from what I observed
    */
@@ -14,75 +14,77 @@ type StreamInfo = {
 }
 
 type SimulcastInfo = {
-  type: string,
-  rid: string,
+  type: string
+  rid: string
   quality: number
 }
 
-type CodecPayloadType = {
-  name: string,
-  type: "audio",
-  priority: number,
-  payload_type: number
-} | {
-  name: string,
-  type: "video",
-  priority: number,
-  payload_type: number,
-  rtx_payload_type: number,
-  encode: boolean,
-  decode: boolean
-}
+type CodecPayloadType =
+  | {
+      name: string
+      type: 'audio'
+      priority: number
+      payload_type: number
+    }
+  | {
+      name: string
+      type: 'video'
+      priority: number
+      payload_type: number
+      rtx_payload_type: number
+      encode: boolean
+      decode: boolean
+    }
 
 export namespace Message {
   // Request messages
   export type Identify = {
-    server_id: string,
-    user_id: string,
-    session_id: string,
-    token: string,
-    video: boolean,
+    server_id: string
+    user_id: string
+    session_id: string
+    token: string
+    video: boolean
     streams: SimulcastInfo[]
   }
 
   export type Resume = {
-    server_id: string,
-    session_id: string,
-    token: string,
+    server_id: string
+    session_id: string
+    token: string
     seq_ack: number
   }
 
   export type Heartbeat = {
-    t: number,
+    t: number
     seq_ack?: number
   }
 
   export type SelectProtocol = {
-    protocol: string,
-    codecs: CodecPayloadType[],
+    protocol: string
+    codecs: CodecPayloadType[]
     data: {
-      address: string,
-      port: number,
+      address: string
+      port: number
       mode: SupportedEncryptionModes
     }
   }
 
   export type Video = {
-    audio_ssrc: number,
-    video_ssrc: number,
-    rtx_ssrc: number,
+    audio_ssrc: number
+    video_ssrc: number
+    rtx_ssrc: number
     streams: {
-      type: "video",
-      rid: string,
-      ssrc: number,
-      active: boolean,
-      quality: number,
-      rtx_ssrc: number,
-      max_bitrate: number,
-      max_framerate: number,
+      type: 'video'
+      rid: string
+      ssrc: number
+      active: boolean
+      quality: number
+      rtx_ssrc: number
+      max_bitrate: number
+      max_framerate: number
       max_resolution: {
-        type: "fixed",
-        width: number,
+        type: 'fixed'
+        width: number
         height: number
       }
     }[]
@@ -94,25 +96,25 @@ export namespace Message {
   }
 
   export type Ready = {
-    ssrc: number,
-    ip: string,
-    port: number,
-    modes: SupportedEncryptionModes[],
-    experiments: string[],
+    ssrc: number
+    ip: string
+    port: number
+    modes: SupportedEncryptionModes[]
+    experiments: string[]
     streams: StreamInfo[]
   }
 
   export type Speaking = {
-    speaking: 0 | 1 | 2,
-    delay: number,
+    speaking: 0 | 1 | 2
+    delay: number
     ssrc: number
   }
 
   export type SelectProtocolAck = {
-    secret_key: number[],
-    audio_codec: string,
-    video_codec: string,
-    mode: string,
+    secret_key: number[]
+    audio_codec: string
+    video_codec: string
+    mode: string
   }
 
   export type HeartbeatAck = {
@@ -122,29 +124,32 @@ export namespace Message {
 
 export namespace GatewayResponse {
   type Generic<Op extends VoiceOpCodes, T extends Record<string, unknown> | null> = {
-    op: Op,
-    d: T,
+    op: Op
+    d: T
     seq?: number
   }
   export type Hello = Generic<VoiceOpCodes.HELLO, Message.Hello>
   export type Ready = Generic<VoiceOpCodes.READY, Message.Ready>
   export type Resumed = Generic<VoiceOpCodes.RESUMED, null>
   export type Speaking = Generic<VoiceOpCodes.SPEAKING, Message.Speaking>
-  export type SelectProtocolAck = Generic<VoiceOpCodes.SELECT_PROTOCOL_ACK, Message.SelectProtocolAck>
+  export type SelectProtocolAck = Generic<
+    VoiceOpCodes.SELECT_PROTOCOL_ACK,
+    Message.SelectProtocolAck
+  >
   export type HeartbeatAck = Generic<VoiceOpCodes.HEARTBEAT_ACK, Message.HeartbeatAck>
 }
 
 export type GatewayResponse =
-  GatewayResponse.Hello |
-  GatewayResponse.Ready |
-  GatewayResponse.Resumed |
-  GatewayResponse.Speaking |
-  GatewayResponse.SelectProtocolAck |
-  GatewayResponse.HeartbeatAck
+  | GatewayResponse.Hello
+  | GatewayResponse.Ready
+  | GatewayResponse.Resumed
+  | GatewayResponse.Speaking
+  | GatewayResponse.SelectProtocolAck
+  | GatewayResponse.HeartbeatAck
 
 export namespace GatewayRequest {
   type Generic<Op extends VoiceOpCodes, T extends Record<string, unknown> | null> = {
-    op: Op,
+    op: Op
     d: T
   }
   export type Identify = Generic<VoiceOpCodes.IDENTIFY, Message.Identify>
@@ -156,9 +161,9 @@ export namespace GatewayRequest {
 }
 
 export type GatewayRequest =
-  GatewayRequest.Identify |
-  GatewayRequest.Resume |
-  GatewayRequest.Heartbeat |
-  GatewayRequest.SelectProtocol |
-  GatewayRequest.Video |
-  GatewayRequest.Speaking
+  | GatewayRequest.Identify
+  | GatewayRequest.Resume
+  | GatewayRequest.Heartbeat
+  | GatewayRequest.SelectProtocol
+  | GatewayRequest.Video
+  | GatewayRequest.Speaking
